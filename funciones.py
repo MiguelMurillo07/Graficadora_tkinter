ceb730d4aecaa14bf8864564a5dc2edb76d4c78b
MiguelMurillo07 <jose2224644@correo.uis.edu.co>
2022-11-24 10:03:01 -0500

    Interfaz Graficadora

--- funciones.py
+++ funciones.py
@@ -4,12 +4,22 @@ import numpy as np
 import matplotlib.pyplot as plt
 from numpy import pi,sin,cos,tan
 N=100
+
 def salir():
     messagebox.showinfo("Suma 1.0", "La app se cerrará..")
     ventana_principal.destroy()
 
+def borrar():
+  messagebox.showinfo("Cálculo v1.0", "Los datos a continuación serán borrados...")
+  a.set("")
+  b.set("")
+  c.set("")
+  d.set("")
+  frame_operaciones.delete("1.0","end")
 
-#Funcion lineal
+
+  
+#Funcion Lineal
 def calcularlineal(m,b,x):
   return int(m.get())*x+int(b.get())
 def mostrar_lineal():
@@ -17,7 +27,7 @@ def mostrar_lineal():
   plt.plot(x,y,color="r")
   plt.xlabel("x")
   plt.ylabel("y")
-  plt.title("Funcion lineal: y=mx+b")
+  plt.title("Función Lineal: y = mx + b")
   plt.grid()
   plt.axhline(y=0,color="b")
   plt.axvline(x=0, color="b")
@@ -25,89 +35,89 @@ def mostrar_lineal():
 def funcion_lineal():
   
   
-  # etiqueta para m
+  # Etiqueta para m
   lb_x = Label(frame_operaciones, text = "m =  ")
-  lb_x.config(bg="pink", fg="blue", font=("Arial",16))
+  lb_x.config(bg="red", fg="blue", font=("Arial",16))
   lb_x.place(x=10, y=20, width=115, height=30)
-  # entry para m
+  # Entry para m
   entry_x = Entry(frame_operaciones, textvariable=m)
   entry_x.config(font=("Arial",20), justify=LEFT, fg="blue")
   entry_x.focus_set()
   entry_x.place(x=135,y=20,width=115, height=30)
-  # etiqueta para b
+  # Etiqueta para b
   lb_x = Label(frame_operaciones, text = "b =  ")
-  lb_x.config(bg="pink", fg="blue", font=("Arial",16))
+  lb_x.config(bg="red", fg="blue", font=("Arial",16))
   lb_x.place(x=10, y=60, width=115, height=30)
-  # entry para b
+  # Entry para b
   entry_x = Entry(frame_operaciones, textvariable=b)
   entry_x.config(font=("Arial",20), justify=LEFT, fg="blue")
   entry_x.focus_set()
   entry_x.place(x=135,y=60,width=115, height=30)
 
-  # etiqueta para c
+  # Etiqueta para c
   lb_x = Label(frame_operaciones, text = "   ")
-  lb_x.config(bg="pink", fg="blue", font=("Arial",16))
+  lb_x.config(bg="red", fg="blue", font=("Arial",16))
   lb_x.place(x=10, y=100, width=115, height=30)
 
-  # entry para c
+  # Entry para c
   lb_x = Label(frame_operaciones, text = "     ")
-  lb_x.config(bg="pink", fg="blue", font=("Arial",16))
+  lb_x.config(bg="red", fg="blue", font=("Arial",16))
   lb_x.place(x=135,y=100,width=115, height=30)
 
-  # etiqueta para d
+  # Etiqueta para d
   lb_x = Label(frame_operaciones, text = "        ")
-  lb_x.config(bg="pink", fg="blue", font=("Arial",16))
+  lb_x.config(bg="red", fg="blue", font=("Arial",16))
   lb_x.place(x=250, y=20, width=115, height=30)
-  # entry para d
+  # Entry para d
   lb_x = Label(frame_operaciones, text = "        ")
-  lb_x.config(bg="pink", fg="blue", font=("Arial",16))
+  lb_x.config(bg="red", fg="blue", font=("Arial",16))
   lb_x.place(x=350,y=20,width=115, height=30)
   
-  #boton cuadratica
+  #Botón Cuadrática
   bt_cuadratica = Button(frame_operaciones, text="Calcular", command=mostrar_lineal)
   bt_cuadratica.place(x=50,y=150, width=100, height=30)
 
-# Funcion cuadratica
+# Función Cuadrática
 def calcular_cuadratica(a,x,b,c):
     return int(a.get())*x**2+int(b.get())*x+int(c.get())
 def funcion_cuadratica():
-  # etiqueta para a
+  # Etiqueta para a
   lb_x = Label(frame_operaciones, text = "a =  ")
-  lb_x.config(bg="pink", fg="blue", font=("Arial",16))
+  lb_x.config(bg="green", fg="blue", font=("Arial",16))
   lb_x.place(x=10, y=20, width=115, height=30)
-  # entry para a
+  # Entry para a
   entry_x = Entry(frame_operaciones, textvariable=a)
   entry_x.config(font=("Arial",20), justify=LEFT, fg="blue")
   entry_x.focus_set()
   entry_x.place(x=135,y=20,width=115, height=30)
-  # etiqueta para b
+  # Etiqueta para b
   lb_x = Label(frame_operaciones, text = "b =  ")
-  lb_x.config(bg="pink", fg="blue", font=("Arial",16))
+  lb_x.config(bg="green", fg="blue", font=("Arial",16))
   lb_x.place(x=10, y=60, width=115, height=30)
-  # entry para b
+  # Entry para b
   entry_x = Entry(frame_operaciones, textvariable=b)
   entry_x.config(font=("Arial",20), justify=LEFT, fg="blue")
   entry_x.focus_set()
   entry_x.place(x=135,y=60,width=115, height=30)
-  # etiqueta para c
+  # Etiqueta para c
   lb_x = Label(frame_operaciones, text = "c =  ")
-  lb_x.config(bg="pink", fg="blue", font=("Arial",16))
+  lb_x.config(bg="green", fg="blue", font=("Arial",16))
   lb_x.place(x=10, y=100, width=115, height=30)
-  # entry para c
+  # Entry para c
   entry_x = Entry(frame_operaciones, textvariable=c)
   entry_x.config(font=("Arial",20), justify=LEFT, fg="blue")
   entry_x.focus_set()
   entry_x.place(x=135,y=100,width=115, height=30)
-  # etiqueta para d
+  # Etiqueta para d
   lb_x = Label(frame_operaciones, text = "        ")
-  lb_x.config(bg="pink", fg="blue", font=("Arial",16))
+  lb_x.config(bg="green", fg="blue", font=("Arial",16))
   lb_x.place(x=250, y=20, width=115, height=30)
-  # entry para d
+  # Entry para d
   lb_x = Label(frame_operaciones, text = "        ")
-  lb_x.config(bg="pink", fg="blue", font=("Arial",16))
+  lb_x.config(bg="green", fg="blue", font=("Arial",16))
   lb_x.place(x=350,y=20,width=115, height=30)
   
-  #boton calcular
+  #Botón calcular
   bt_cuadratica = Button(frame_operaciones, text="Calcular", command=mostrar_cuadratica)
   bt_cuadratica.place(x=50,y=150, width=100, height=30)
 
@@ -116,7 +126,7 @@ def mostrar_cuadratica():
   plt.plot(x,y,color="r")
   plt.xlabel("x")
   plt.ylabel("y")
-  plt.title("Funcion cuadratica: y=ax**2+bx+c")
+  plt.title("Función Cuadrática: ax² + bx + c = 0")
   plt.grid()
   plt.axhline(y=0,color="b")
   plt.axvline(x=0, color="b")
@@ -128,58 +138,60 @@ def mostrar_cubica():
   plt.plot(x,y,color="r")
   plt.xlabel("x")
   plt.ylabel("y")
-  plt.title("Funcion cubica: y=ax**3+bx**2+cx+d")
+  plt.title("Funcion Cúbica: ax³ + bx² + c + d")
   plt.grid()
   plt.axhline(y=0,color="b")
   plt.axvline(x=0, color="b")
   plt.show() 
+
+# Función Cúbica
 
 def funcion_cubica():
   
   lb_x = Label(frame_operaciones, text = "a =  ")
   lb_x.config(bg="pink", fg="blue", font=("Arial",16))
   lb_x.place(x=10, y=20, width=115, height=30)
-  # entry para a
+  # Entry para a
   entry_x = Entry(frame_operaciones, textvariable=a)
   entry_x.config(font=("Arial",20), justify=LEFT, fg="blue")
   entry_x.focus_set()
   entry_x.place(x=135,y=20,width=115, height=30)
-  # etiqueta para b
+  # Etiqueta para b
   lb_x = Label(frame_operaciones, text = "b =  ")
   lb_x.config(bg="pink", fg="blue", font=("Arial",16))
   lb_x.place(x=10, y=60, width=115, height=30)
-  #entry para b
+  # Entry para b
   entry_x = Entry(frame_operaciones, textvariable=b)
   entry_x.config(font=("Arial",20), justify=LEFT, fg="blue")
   entry_x.focus_set()
   entry_x.place(x=135,y=60,width=115, height=30)
-  # etiqueta para c
+  # Etiqueta para c
   lb_x = Label(frame_operaciones, text = "c =  ")
   lb_x.config(bg="pink", fg="blue", font=("Arial",16))
   lb_x.place(x=10, y=100, width=115, height=30)
-  # entry para c
+  # Entry para c
   entry_x = Entry(frame_operaciones, textvariable=c)
   entry_x.config(font=("Arial",20), justify=LEFT, fg="blue")
   entry_x.focus_set()
   entry_x.place(x=135,y=100,width=115, height=30)
 
-  # etiqueta para d
+  # Etiqueta para d
   lb_x = Label(frame_operaciones, text = "d =  ")
   lb_x.config(bg="pink", fg="blue", font=("Arial",16))
   lb_x.place(x=250, y=20, width=115, height=30)
-  # entry para d
+  # Entry para d
   entry_x = Entry(frame_operaciones, textvariable=d)
   entry_x.config(font=("Arial",20), justify=LEFT, fg="blue")
   entry_x.focus_set()
   entry_x.place(x=350,y=20,width=115, height=30)
 
-  #boton calcular
+  # Botón Calcular
   bt_cuadratica = Button(frame_operaciones, text="Calcular", command=mostrar_cubica)
   bt_cuadratica.place(x=50,y=150, width=100, height=30)
 
 
+# Función Exponencial
 
-  
 def calcular_expotencial(x,a):
   return int(a.get())**x
 def funcion_expotencial():
@@ -187,36 +199,36 @@ def funcion_expotencial():
   lb_x = Label(frame_operaciones, text = "a =  ")
   lb_x.config(bg="pink", fg="blue", font=("Arial",16))
   lb_x.place(x=10, y=20, width=115, height=30)
-  # entry para a
+  # Entry para a
   entry_x = Entry(frame_operaciones, textvariable=a)
   entry_x.config(font=("Arial",20), justify=LEFT, fg="blue")
   entry_x.focus_set()
   entry_x.place(x=135,y=20,width=115, height=30)
-  # etiqueta para b
+  # Etiqueta para b
   lb_x = Label(frame_operaciones, text = "          ")
   lb_x.config(bg="pink", fg="blue", font=("Arial",16))
   lb_x.place(x=10, y=60, width=115, height=30)
-  #entry para b
+  # Entry para b
   lb_x = Label(frame_operaciones, text = "          ")
   lb_x.config(bg="pink", fg="blue", font=("Arial",16))
   lb_x.place(x=135,y=60,width=115, height=30)
-  # etiqueta para c
+  # Etiqueta para c
   lb_x = Label(frame_operaciones, text = "           ")
   lb_x.config(bg="pink", fg="blue", font=("Arial",16))
   lb_x.place(x=10, y=100, width=115, height=30)
-  # entry para c
+  # Entry para c
   lb_x = Label(frame_operaciones, text = "           ")
   lb_x.config(bg="pink", fg="blue", font=("Arial",16))
   lb_x.place(x=135,y=100,width=115, height=30)
-  # etiqueta para d
+  # Etiqueta para d
   lb_x = Label(frame_operaciones, text = "            ")
   lb_x.config(bg="pink", fg="blue", font=("Arial",16))
   lb_x.place(x=250, y=20, width=115, height=30)
-  # entry para d
+  # Entry para d
   lb_x = Label(frame_operaciones, text = "            ")
   lb_x.config(bg="pink", fg="blue", font=("Arial",16))
   lb_x.place(x=350,y=20,width=115, height=30)
-  #boton calcular
+  #Botón Calcular
   bt_cuadratica = Button(frame_operaciones, text="Calcular", command=mostrar_expotencial)
   bt_cuadratica.place(x=50,y=150, width=100, height=30)
 def mostrar_expotencial():
@@ -224,13 +236,14 @@ def mostrar_expotencial():
   plt.plot(x,y,color="r")
   plt.xlabel("x")
   plt.ylabel("y")
-  plt.title("Funcion cubica: y=ax**3+bx**2+cx+d")
+  plt.title("Función Exponencial: f(x) = b**x")
   plt.grid()
   plt.axhline(y=0,color="b")
   plt.axvline(x=0, color="b")
   plt.show() 
   
-# funcion logaritmo
+#Función Logaritmo
+
 def funcion_logaritmica(x):
   return np.log(x)
 def mostrar_logaritmo():
@@ -238,50 +251,53 @@ def mostrar_logaritmo():
   plt.plot(x,y,color="r")
   plt.xlabel("x")
   plt.ylabel("y")
-  plt.title("Funcion Logaritmica Y=log(x)")
+  plt.title("Función Logarítmica: f(x)=loga(x)")
   plt.grid()
   plt.axhline(y=0,color="b")
   plt.axvline(x=0, color="b")
   plt.show()
+
+# Función Trigonométrica
+
 def funcion_trigonometrica():
   lb_x = Label(frame_operaciones, text = "        ")
-  lb_x.config(bg="pink", fg="blue", font=("Arial",16))
+  lb_x.config(bg="brown", fg="blue", font=("Arial",16))
   lb_x.place(x=10, y=20, width=115, height=30)
-  # entry para a
+  # Entry para a
   lb_x = Label(frame_operaciones, text = "        ")
-  lb_x.config(bg="pink", fg="blue", font=("Arial",16))
+  lb_x.config(bg="brown", fg="blue", font=("Arial",16))
   lb_x.place(x=135,y=20,width=115, height=30)
   
-  # etiqueta para b
+  # Etiqueta para b
   lb_x = Label(frame_operaciones, text = "          ")
-  lb_x.config(bg="pink", fg="blue", font=("Arial",16))
+  lb_x.config(bg="brown", fg="blue", font=("Arial",16))
   lb_x.place(x=10, y=60, width=115, height=30)
-  #entry para b
+  # Entry para b
   lb_x = Label(frame_operaciones, text = "          ")
-  lb_x.config(bg="pink", fg="blue", font=("Arial",16))
+  lb_x.config(bg="brown", fg="blue", font=("Arial",16))
   lb_x.place(x=135,y=60,width=115, height=30)
-  # etiqueta para c
+  # Etiqueta para c
   lb_x = Label(frame_operaciones, text = "           ")
-  lb_x.config(bg="pink", fg="blue", font=("Arial",16))
+  lb_x.config(bg="brown", fg="blue", font=("Arial",16))
   lb_x.place(x=10, y=100, width=115, height=30)
-  # entry para c
+  # Entry para c
   lb_x = Label(frame_operaciones, text = "           ")
-  lb_x.config(bg="pink", fg="blue", font=("Arial",16))
+  lb_x.config(bg="brown", fg="blue", font=("Arial",16))
   lb_x.place(x=135,y=100,width=115, height=30)
-  # etiqueta para d
+  # Etiqueta para d
   lb_x = Label(frame_operaciones, text = "            ")
-  lb_x.config(bg="pink", fg="blue", font=("Arial",16))
+  lb_x.config(bg="brown", fg="blue", font=("Arial",16))
   lb_x.place(x=250, y=20, width=115, height=30)
-  # entry para d
+  # Entry para d
   lb_x = Label(frame_operaciones, text = "            ")
-  lb_x.config(bg="pink", fg="blue", font=("Arial",16))
+  lb_x.config(bg="brown", fg="blue", font=("Arial",16))
   lb_x.place(x=350,y=20,width=115, height=30)
-  #boton calcular
+  # Botón Calcular
   lb_x = Label(frame_operaciones, text = "            ")
-  lb_x.config(bg="pink", fg="blue", font=("Arial",16))
+  lb_x.config(bg="brown", fg="blue", font=("Arial",16))
   lb_x.place(x=50,y=150, width=100, height=30)
 
-  #boton seno
+  #Botón Seno
   
   bt_seno = Button(frame_operaciones, text="sen(x)", command=mostrar_seno)
   bt_seno.place(x=50,y=150, width=100, height=30)
@@ -300,7 +316,7 @@ def mostrar_seno():
   plt.plot(x,y,color="r")
   plt.xlabel("x")
   plt.ylabel("y")
-  plt.title("Funcion sen(x)")
+  plt.title("Función Sen(x)")
   plt.grid()
   plt.axhline(y=0,color="b")
   plt.axvline(x=0, color="b")
@@ -313,7 +329,7 @@ def mostrar_coseno():
   plt.plot(x,y,color="r")
   plt.xlabel("x")
   plt.ylabel("y")
-  plt.title("Funcion cos(x)")
+  plt.title("Función Cos(x)")
   plt.grid()
   plt.axhline(y=0,color="b")
   plt.axvline(x=0, color="b")
@@ -325,7 +341,7 @@ def mostrar_tangente():
   plt.plot(x,y,color="r")
   plt.xlabel("x")
   plt.ylabel("y")
-  plt.title("Funcion tan(x)")
+  plt.title("Función Tan(x)")
   plt.grid()
   plt.axhline(y=0,color="b")
   plt.axvline(x=0, color="b")
@@ -341,7 +357,7 @@ x= np.linspace(-10,10, num=N)
 
 
 # titulo de la ventana
-ventana_principal.title("Funciones")
+ventana_principal.title("Funciones Matemáticas")
 
 # tamañan de la ventana
 ventana_principal.geometry("500x500")
@@ -350,41 +366,53 @@ ventana_principal.geometry("500x500")
 ventana_principal.resizable(0,0)
 
 # color de fondo de la ventana
-ventana_principal.config(bg= "black")
+ventana_principal.config(bg= "purple")
 
-# etiqueta para el titulo de la app
-titulo = Label(ventana_principal, text= "----Funciones----")
-titulo.config(bg="black", fg="white", font=("Arial",18))
+# Etiqueta para el titulo de la app
+titulo = Label(ventana_principal, text= "Graficadora de Funciones")
+titulo.config(bg="LightBlue4", fg="white", font=("Arial",18))
 titulo.place(x=50,y=10, width=400, height=40)
-# boton lineal
+# Botón Lineal
 bt_lineal = Button(ventana_principal, text="Lineal", command=funcion_lineal)
 bt_lineal.place(x=200,y=60, width=100, height=30)
 
-#boton cuadratica
-bt_cuadratica = Button(ventana_principal, text="Cuadratica", command=funcion_cuadratica)
+#Botón Cuadrática
+bt_cuadratica = Button(ventana_principal, text="Cuadrática", command=funcion_cuadratica)
 bt_cuadratica.place(x=200,y=95, width=100, height=30)
 
-#boton cubica
-bt_cubica= Button(ventana_principal, text="Cubica", command=funcion_cubica)
+#Botón Cúbica
+bt_cubica= Button(ventana_principal, text="Cúbica", command=funcion_cubica)
 bt_cubica.place(x=200,y=130, width=100, height=30)
 
-#boton expotencial
-bt_expotencial= Button(ventana_principal, text="Expotencial", command=funcion_expotencial)
+#Botón Expotencial
+bt_expotencial= Button(ventana_principal, text="Exponencial", command=funcion_expotencial)
 bt_expotencial.place(x=200,y=165, width=100, height=30)
 
-#funcion logaritmo
-bt_logaritmo= Button(ventana_principal, text="logaritmica", command=mostrar_logaritmo)
+#Botón logaritmo
+bt_logaritmo= Button(ventana_principal, text="Logarítmica", command=mostrar_logaritmo)
 bt_logaritmo.place(x=200,y=200, width=100, height=30)
 
-#funcion trigonometrica
-bt_trigonometrica= Button(ventana_principal, text="trigonometrica", command=funcion_trigonometrica)
+#Botón Trigonométrica
+bt_trigonometrica= Button(ventana_principal, text="Trigonométrica", command=funcion_trigonometrica)
 bt_trigonometrica.place(x=200,y=235, width=100, height=30)
 
 # ------------------------
-# frame operaciones
+# Frame Operaciones
 # ------------------------
 frame_operaciones = Frame(ventana_principal)
-frame_operaciones.config(bg="pink", width=480, height=190)
+frame_operaciones.config(bg="goldenrod", width=480, height=190)
 frame_operaciones.place(x=10,y=300)
+
+
+# Boton Borrar
+bt_borrar = Button(ventana_principal, text="Borrar", command=borrar)
+bt_borrar.place(x=40, y=250, width=100, height=30)
+
+
+
+# Botón Salir
+bt_salir = Button(ventana_principal, text="Salir", command=salir)
+bt_salir.place(x=370, y=250, width=100,height=30)
+
 
 ventana_principal.mainloop()
